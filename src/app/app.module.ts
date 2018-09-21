7128ba457c404b7515a76d0135ed70b4809b334f
Sarah Colby <scolby@austin.utexas.edu>
2018-09-21 13:19:03 -0500

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
@@ -18,9 +20,6 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     FormsModule,
     AppRoutingModule
-  ],
-  providers: [
-    // no need to place any providers due to the `providedIn` flag...
   ],
   bootstrap: [ AppComponent ]
 })
