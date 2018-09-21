11192b459bb21746e3027e069cf3a3fb1c8fa8b7
Sarah Colby <vernistage@users.noreply.github.com>
2018-09-21 15:34:37 -0500

    Routing (#4)
    * start of app routing
    
    * add router
    
    * add dashboard component
    
    * Dashboard WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
-  ],
-  providers: [
-    // no need to place any providers due to the `providedIn` flag...
+    FormsModule,
+    AppRoutingModule
   ],
   bootstrap: [ AppComponent ]
 })
