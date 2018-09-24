935b4e8d71823fe5a67b75b6640e96c888ddbd4d
Sarah Colby <vernistage@users.noreply.github.com>
2018-09-24 15:03:57 -0500

    Routing (#5)
    * start of app routing
    
    * add router
    
    * add dashboard component
    
    * Dashboard WIP
    
    * fix dashboard
    
    * finish

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent }      from './heroes/heroes.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 
+import { DashboardComponent }   from './dashboard/dashboard.component';
+
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent }
+  { path: 'heroes', component: HeroesComponent },
 ];
 
 @NgModule({
